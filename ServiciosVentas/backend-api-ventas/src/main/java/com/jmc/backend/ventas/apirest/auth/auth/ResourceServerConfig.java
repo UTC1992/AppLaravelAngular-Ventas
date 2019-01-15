a5af9678d9e6007bbafc544bb26339a3e6b597c1
desarrolloecuador <38869442+desarrolloecuador@users.noreply.github.com>
2019-01-15 15:43:43 -0500

    Cors Configuration
    configuracio de cors para angular

--- ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/auth/auth/ResourceServerConfig.java
+++ ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/auth/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.jmc.backend.ventas.apirest.auth.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -21,7 +30,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, "/api/empresa").hasRole("ADMIN")
 		.antMatchers(HttpMethod.GET, "/api/empresa/admin/{id}").hasRole("ROOT")
 		.antMatchers(HttpMethod.GET, "/auth/usuario/roles/{id}").hasAnyRole("USER","ADMIN")
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().cors().configurationSource(corsConfigurationSource());
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config= new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+		config.setAllowCredentials(true);
+		config.setAllowedHeaders(Arrays.asList("Content-Type","Autorization"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+		
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+		
 	}
 
 	
