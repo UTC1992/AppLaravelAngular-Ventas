a9280c742eceb4ead9e880e688f4acf8b803e14e
omar <omar.guanoluisa25@gmail.com>
2019-01-16 11:34:04 -0500

    commit 5 configuracion del cors para conexion con el angular

--- ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/auth/auth/ResourceServerConfig.java
+++ ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/auth/auth/ResourceServerConfig.java
@@ -1,10 +1,20 @@
 package com.jmc.backend.ventas.apirest.auth.auth;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -21,7 +31,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, "/api/empresa").hasRole("ADMIN")
 		.antMatchers(HttpMethod.GET, "/api/empresa/admin/{id}").hasRole("ROOT")
 		.antMatchers(HttpMethod.GET, "/auth/usuario/roles/{id}").hasAnyRole("USER","ADMIN")
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().cors().configurationSource(corsConfigurationSource());
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+		
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+		
 	}
 
 	
--- ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/controllers/ClienteRestController.java
+++ ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/controllers/ClienteRestController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jmc.backend.ventas.apirest.models.entity.Cliente;
 import com.jmc.backend.ventas.apirest.models.services.IClienteService;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
