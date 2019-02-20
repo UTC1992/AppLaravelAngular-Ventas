26d74868d1cead6f0945752dff0785a2396656df
desarrolloecuador <38869442+desarrolloecuador@users.noreply.github.com>
2019-02-20 09:33:56 -0500

    Bodega sucursal
    Bodega

--- ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/models/dao/IBodegaSucursalDao.java
+++ ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/models/dao/IBodegaSucursalDao.java
@@ -1,10 +1,17 @@
 package com.jmc.backend.ventas.apirest.models.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jmc.backend.ventas.apirest.models.entity.BodegaSucursal;
+import com.jmc.backend.ventas.apirest.models.entity.PuntoVenta;
 
 public interface IBodegaSucursalDao extends JpaRepository<BodegaSucursal, Long> {
 
+	
+	@Query("select b from BodegaSucursal b where b.punto=?1")
+	public List<BodegaSucursal> findByPuntoVentaQuery(PuntoVenta punto);
 	
 }
--- ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/models/services/BodegaSucursalServiceImplement.java
+++ ServiciosVentas/backend-api-ventas/src/main/java/com/jmc/backend/ventas/apirest/models/services/BodegaSucursalServiceImplement.java
@@ -2,12 +2,15 @@ package com.jmc.backend.ventas.apirest.models.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jmc.backend.ventas.apirest.models.dao.IBodegaSucursalDao;
 import com.jmc.backend.ventas.apirest.models.entity.BodegaSucursal;
+import com.jmc.backend.ventas.apirest.models.entity.PuntoVenta;
 import com.jmc.backend.ventas.apirest.models.services.Interfaces.IBodegaSucursalService;
+
 
 @Service
 public class BodegaSucursalServiceImplement implements IBodegaSucursalService{
@@ -25,6 +28,19 @@ public class BodegaSucursalServiceImplement implements IBodegaSucursalService{
 	public BodegaSucursal save(BodegaSucursal bodega) {
 		// TODO Auto-generated method stub
 		return bodegaDao.save(bodega);
+	}
+
+
+	@Override
+	public List<BodegaSucursal> findAllByPuntoVenta(PuntoVenta punto) {
+		// TODO Auto-generated method stub
+		return bodegaDao.findByPuntoVentaQuery(punto);
+	}
+
+	@Override
+	public BodegaSucursal findById(Long id) {
+		// TODO Auto-generated method stub
+		return bodegaDao.findById(id).orElse(null);
 	}
 
 }
